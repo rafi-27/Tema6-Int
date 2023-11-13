f6235bf4f8c55bfa41dc9b38cc3a9af2f93000bb
rafi-27 <rafikbachri21@gmail.com>
2023-11-13 22:37:28 +0100

    13/11/23

--- src/EjerciciosB/RectanguloBMain.java
+++ src/EjerciciosB/RectanguloBMain.java
@@ -3,10 +3,24 @@ package EjerciciosB;
 public class RectanguloBMain {
     public static void main(String[] args) {
 
-        RectanguloB RectanguloUno = new RectanguloB(4,5,5,2);
-        RectanguloB RectanguloDos = new RectanguloB(5,6,2,5);
+        RectanguloB rectanguloUno = new RectanguloB(1,2,5,5);
+        RectanguloB rectanguloDos = new RectanguloB(1,1,2,5);
 
-        int lados =
+        System.out.println("Coordenadas 1: "+"("+"x:"+rectanguloUno.x1+","+"y:"+rectanguloUno.y1+")"+" "+"("+"x:"+rectanguloUno.x2+","+"y:"+rectanguloUno.y2+")");
+        System.out.println("Coordenadas 2: "+"("+"x:"+rectanguloDos.x1+","+"y:"+rectanguloDos.y1+")"+" "+"("+"x:"+rectanguloDos.x2+","+"y:"+rectanguloDos.y2+")");
+
+        System.out.println("Procedemos a calcular el area y perimetro-->");
+        int ladoReUno = rectanguloUno.x2-rectanguloUno.x1;
+        int alturaReUno = rectanguloUno.y2-rectanguloUno.y1;
+        int sumaLadoUno = (ladoReUno*2)+(alturaReUno*2);
+        int perimetroUno = ladoReUno*alturaReUno;
+        System.out.println("Del primer rectangulo, los lados suman:"+sumaLadoUno+" y tiene un perimetro de "+perimetroUno);
+
+        int ladoReDos = rectanguloDos.x2-rectanguloDos.x1;
+        int alturaReDos = rectanguloDos.y2-rectanguloDos.y1;
+        int sumaLadoDos = (ladoReDos*2)+(alturaReDos*2);
+        int perimetroDos = ladoReDos*alturaReDos;
+        System.out.println("Del segundo rectangulo, los lados suman:"+sumaLadoDos+" y tiene un perimetro de "+perimetroDos);
 
     }
 }
